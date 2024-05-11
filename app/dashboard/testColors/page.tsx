e8a70e18e1217d29666134e86e228e90bbbef99e
Luiz Vitor Karpinski <luizv.karpinski@unochapeco.edu.br>
2024-05-11 00:15:43 -0300

    title pagina edição de faturas

--- app/dashboard/testColors/page.tsx
+++ app/dashboard/testColors/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from "next"
 import getAllColors, { Palette } from "../../lib/tests"
 import MyPalette from "./myPalette"
+
+export const metadata: Metadata = {
+    title: "Palette Color"
+}
 
 export default async function Page() {
     const palette: Palette = await getAllColors()
